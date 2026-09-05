1fec4da7299f848f249af7ecedf6149a82a8f43f
Nico <nikitopedroza2013@gmail.com>
2026-09-04 23:09:20 -0300

    creacion del modelo de la tabla binance_keys

---
+++ models/binancekeys.py
@@ -0,0 +1,14 @@
+import uuid
+from sqlalchemy import Column, String, Boolean, ForeignKey, Text
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.sql import func
+from database import Base
+
+class Users(Base):
+    __tablename__ = "binance_keys"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    user_id = Column(UUID(as_uuid=True), ForeignKey("users.id"), nullable=False)
+    api_key = Column(Text, nullable=False)
+    api_secret = Column(Text, nullable=False)
+    alias = Column(String(50))
--- models/users.py
+++ models/users.py
@@ -1,11 +1,13 @@
-from sqlalchemy import Column, Integer, String, Boolean, TIMESTAMP
+import uuid
+from sqlalchemy import Column, String, Boolean, TIMESTAMP
+from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.sql import func
 from database import Base
- 
+
 class Users(Base):
     __tablename__ = "users"
- 
-    id = Column(Integer, primary_key=True, nullable=False)
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     username = Column(String(50), nullable=False)
     password_hash = Column(String(255), nullable=False)
     is_active = Column(Boolean, default=True)
