883874103b4c1e75c4a0c6e50bd9daace66ecc30
Nico <nikitopedroza2013@gmail.com>
2026-09-05 20:07:08 -0300

    actualizacion de inicializacion de datos, se podran subir las apikey directamente

--- init_db.py
+++ init_db.py
@@ -1,11 +1,11 @@
 import asyncio
 from getpass import getpass
-
+from sqlalchemy.ext.asyncio import AsyncSession
 from database import AsyncSessionLocal
 from models import Users, BinanceKeys
-from auth import pwd_context
+from auth import pwd_context, encrypt_key
 
-async def crear_usuario(db, username: str, password: str):
+async def crear_usuario(db:AsyncSession , username: str, password: str):
     password_hash = pwd_context.hash(password)
     nuevo_usuario = Users(
         username=username,
@@ -13,9 +13,27 @@ async def crear_usuario(db, username: str, password: str):
         is_active=True,
     )
     db.add(nuevo_usuario)
-    await db.commit()
+    await db.flush()
+    await db.refresh(nuevo_usuario)
     print(f"Usuario '{username}' creado correctamente.")
+    return nuevo_usuario
 
+async def subir_api_key(db:AsyncSession, user: Users):
+    alias = input("Alias de las Keys: ").strip()
+    public_key = getpass("Public Key: ").strip()
+    private_key = getpass("Private Key: ").strip()
+
+    public_key = encrypt_key(public_key)
+    private_key = encrypt_key(private_key)
+    new_binance_apis = BinanceKeys(
+        user_id=user.id,
+        api_key=public_key,
+        api_secret=private_key,
+        alias=alias
+    )
+    db.add(new_binance_apis)
+    await db.flush()
+    
 
 async def main():
     async with AsyncSessionLocal() as db:
@@ -28,7 +46,15 @@ async def main():
             print("Las contraseñas no coinciden. Abortando.")
             return
 
-        await crear_usuario(db, username, password)
+        nuevo_usuario = await crear_usuario(db, username, password)
+        opcion = input("Desea ingresar las API_KEY de Binance?:(Y/N)")
+        if opcion.lower() in ["y","yes"]:
+            await subir_api_key(db, nuevo_usuario)
+
+        await db.commit()
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except (KeyboardInterrupt, EOFError):
+        print("\n=== PROGRAMA TERMINADO POR EL USUARIO ===")
